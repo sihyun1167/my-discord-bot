3d8ced5750dec5cbce1cf66ce15148cb708a2b71
sihyun1167 <sihyun1167@gmail.com>
2026-09-18 23:34:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ import json
 import os
 import re
 from flask import Flask
-⁠import threading
+import threading
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+threading.Thread(target=run_web, daemon=True).start()
 
 # ==========================================
 # 1. 봇 기본 설정
